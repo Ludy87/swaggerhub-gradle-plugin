9102371d5a1da3ba9f008b51cbbddb43f45c6a34
Ludy87 <Ludy87@users.noreply.github.com>
2025-03-15 21:12:51 +0100

    Update DebugLogger.java

--- src/main/java/io/swagger/swaggerhub/v2/DebugLogger.java
+++ src/main/java/io/swagger/swaggerhub/v2/DebugLogger.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 
 public class DebugLogger {
     public static void info(String message) {
-        System.out.println(message); // Falls Gradle es doch anzeigt
+        System.out.println(message);
         try (PrintWriter out = new PrintWriter(new FileWriter("test-output.log", true))) {
             out.println(message);
         } catch (IOException e) {
